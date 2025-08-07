f32ca80c594fd15c3ccd5f117804948a06033bf8
Victor <controleaoquadrado@gmail.com>
2025-08-07 16:20:05 -0300

    Excessões em php

--- Integrando PHP com banco de dados/Excessões/script.php
+++ Integrando PHP com banco de dados/Excessões/script.php
@@ -8,8 +8,17 @@ define('USER', 'user');
 define('PASSWORD', 'psswd');
 
 try {
-	$dsn = new PDO("mysql:host=". HOST . ";port=".PORT.";dbname=" . DBNAME .";user=" . USER . ";password=" . PASSWORD);
+	$dsn = new PDO(
+		"mysql:host=". HOST . 
+		";port=".PORT.";dbname=" . DBNAME .
+		";user=" . USER .
+		";password=" . PASSWORD,
+		array(PDO::ATTR_PERSISTENT => true) //Essa linha faz com que a conexão seja permanente
+	);
+
 } catch (PDOException $e) {
 	echo 'A conexão falhou e retornou a seguinte mensagem de erro: ' .$e->getMessage();
 }
+
+$dsn = null; // O encerramento da conexão aberta
 ?>
